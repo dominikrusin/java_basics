818fd933391253828e3001032fe0944cdc20d067
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-19 19:58:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "kwiecień";
+        int year = 2022;
+        double day = 19;
+        System.out.println("miesiąc: " + month);
+        System.out.println("rok: " + year);
+        System.out.println("dzień: " + day);
+    }
+
+}
