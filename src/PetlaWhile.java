d91781ebab696aef989e557f7e55d7c5308c803d
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-20 20:17:06 +0200

    for

---
+++ src/PetlaWhile.java
@@ -0,0 +1,10 @@
+public class PetlaWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        while(a < 10){
+            System.out.println("Marek jest super");
+            a++;
+        }
+        System.out.println("koniec pętli");
+    }
+}
